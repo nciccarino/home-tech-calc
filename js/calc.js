1300a2d654af947ef2a48a8134326bf6ca10de62
Nicholas A Ciccarino <nciccarino@gmail.com>
2018-07-13 15:01:44 -0400

    updates

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,11 @@
 		$(document).ready(function() { 
 
+			var typeTab = 1; 
+
+			var yr = new Date().getFullYear()
+			console.log(yr); 
+			$("#year").html(yr); 
+
 			function handleDrop() {
 				var cat = $("#catSelect").val();
 				var typ = $("#typeSelect").val();
@@ -7,13 +13,19 @@
 				console.log(cat)
 
 				if (cat == 2 || cat == 4) {
-					$("#CabBudget").val(30);
-					$("#FloorBudget").val(8);
-					$("#CounterBudget").val(10);
-					$("#AppBudget").val(25);
-					$("#LightingBudget").val(8);
-					$("#WallBudget").val(3);
-					$("#LaborBudget").val(15);
+					$(".shower").css("display", "table-cell");
+					$("#walls").html("Painting"); 
+					$("#floor").html("Tile (Floor + Bath Surround)")
+					$("#plumb").html("Plumbing Fixtures"); 
+
+					$("#CabBudget").val(17);
+					$("#FloorBudget").val(26);
+					$("#CounterBudget").val(9);
+					$("#AppBudget").val(15);
+					$("#LightingBudget").val(10);
+					$("#WallBudget").val(5);
+					$("#GlassBudget").val(7);
+					$("#LaborBudget").val(10);
 					$("#MiscBudget").val(1);
 					if (typ == 0) {
 						$("#PercentHome").val(7);
@@ -23,12 +35,18 @@
 					}
 				} 
 				if (cat == 0) {
+					$(".shower").css("display", "none");
+					$("#walls").html("Wall Covering");
+					$("#floor").html("Flooring"); 
+					$("#plumb").html("Appliances");
+
 					$("#CabBudget").val(40);
 					$("#FloorBudget").val(7);
 					$("#CounterBudget").val(12);
 					$("#AppBudget").val(8);
 					$("#LightingBudget").val(4);
 					$("#WallBudget").val(3);
+					$("#GlassBudget").val(0);
 					$("#LaborBudget").val(25);
 					$("#MiscBudget").val(1);
 					// $("#PercentHome").val(12);
@@ -70,17 +88,24 @@
 					TotalBudget = (document.getElementById('budget').value)
 				} else {
 					TotalBudget = ProjBudget
-					// document.getElementById('budget').value = TotalBudget
 				}
 
-				var Cabinet = TotalBudget * (document.getElementById('CabBudget').value / 100)
-				var Flooring = TotalBudget * (document.getElementById('FloorBudget').value / 100)
-				var Counter = TotalBudget * (document.getElementById('CounterBudget').value / 100)
-				var Appliance = TotalBudget * (document.getElementById('AppBudget').value / 100)
-				var Lighting = TotalBudget * (document.getElementById('LightingBudget').value / 100)
-				var Wall = TotalBudget * (document.getElementById('WallBudget').value / 100)
-				var Labor = TotalBudget * (document.getElementById('LaborBudget').value / 100)
-				var Misc = TotalBudget * (document.getElementById('MiscBudget').value / 100)
+				// if(typeTab = 0) {
+				// 	TotalBudget = ProjBudget
+				// }
+				// if(typeTab = 1) {
+				// 	TotalBudget = (document.getElementById('budget').value)
+				// }
+
+				var Cabinet = TotalBudget * (document.getElementById('CabBudget').value / 100);
+				var Flooring = TotalBudget * (document.getElementById('FloorBudget').value / 100);
+				var Counter = TotalBudget * (document.getElementById('CounterBudget').value / 100);
+				var Appliance = TotalBudget * (document.getElementById('AppBudget').value / 100);
+				var Lighting = TotalBudget * (document.getElementById('LightingBudget').value / 100);
+				var Wall = TotalBudget * (document.getElementById('WallBudget').value / 100);
+				var Glass = TotalBudget * (document.getElementById('GlassBudget').value / 100);
+				var Labor = TotalBudget * (document.getElementById('LaborBudget').value / 100);
+				var Misc = TotalBudget * (document.getElementById('MiscBudget').value / 100);
 
 				$("#cabinetCalc").html("$ " + Cabinet.toFixed(2).toLocaleString());
 				$("#flooringCalc").html("$ " + Flooring.toFixed(2).toLocaleString());
@@ -88,6 +113,7 @@
 				$("#applCalc").html("$ " + Appliance.toFixed(2).toLocaleString());
 				$("#lightCalc").html("$ " + Lighting.toFixed(2).toLocaleString());
 				$("#wallCalc").html("$ " + Wall.toFixed(2).toLocaleString());
+				$("#glassCalc").html("$ " + Glass.toFixed(2).toLocaleString()); 
 				$("#laborCalc").html("$ " + Labor.toFixed(2).toLocaleString());
 				$("#miscCalc").html("$ " + Misc.toFixed(2).toLocaleString());
 
@@ -101,9 +127,31 @@
 					var newHome = valHome + netVal; 
 					console.log(newHome)
 					$("#homeCalc").html("<b>$ " + newHome.toFixed(2).toLocaleString() + "</b>")
+
+					$("#roiCalc").html("<b>$ " + netVal.toFixed(2).toLocaleString() + "</b>")
 				}
 
-				$("#roiCalc").html("<b>$ " + netVal.toFixed(2).toLocaleString() + "</b>")
+				var location = $("#locSelect").find(":selected").text(); 
+				var projTypee = $("#catSelect").val(); 
+				var projType = $("#catSelect").find(":selected").text();
+				var qual = $("#typeSelect").find(":selected").text();
+
+				$(".callButtonKitchen").css("display", "none");
+				$("#resetCalc").css("display", "block");
+
+				var parKitchen = "The kitchen is the entertainment center of any home. Therefore, it is consistently one of the most satisfying return on investments in a home improvement. An investment that adds appreciating value to your home and quality of living. The kitchen is one of the top considerations for home buyers when considering a home purchase. 3 out of 4 potential home buyers will purchase a home with a newer kitchen over a home without an updated kitchen.";
+				var parBath = "Homeowners regularly use bathroom more than any other room. Most homeowners do not take long baths, and opt for a large multi-function shower environment.	Master baths are truly created around very personal aesthetics and truly become a sanctuary of admiration and pleasure. They are one of the top return on investments you can make in your home. Improve water pressure and showering experience with upgraded shower fixtures. High quality tile, stone, and fixtures have enduring value and reflect desirable factors when time to sell… Make your bath a vacation!";
+
+				if (projTypee == 2 || projTypee == 4) { 
+					$(".paragraph").html(parBath);
+				}
+				if (projTypee == 0) {
+					$(".paragraph").html(parKitchen);
+				}
+
+				var res = "Based on your budget of <span><b>$" + ProjBudget.toFixed(2).toLocaleString() + "</b></span>, a new <span><b>" + projType + "</b></span> in <span><b>" + location + "</b></span> with custom <span><b>" + qual + "</b></span> features will have an estimated return on investment of <span><b>$" + netVal.toFixed(2).toLocaleString() + "</b></span>, increasing your home value to <span><b>$" + newHome.toFixed(2).toLocaleString() + "</b></span>!";
+
+				$(".rescalc").html(res);
 
 			}
 
@@ -137,11 +185,26 @@
 				if(reduced <= 100) {
 					$("#totalCalc").html(reduced + " %")
 				}
-
 			}
 
 			$(document.body).on("click", '.callButtonKitchen', TotalCostKitchen);
 			$(document.body).on("click", ".breakdown", handleTotal); 
 			$(document.body).on("click", ".dropSelect", handleDrop);
 
-		})
+			$("#home-tab").on("click", function(){
+				$("#budgetTable").css("display", "none");
+				$("#homeValueTable").css("display", "block");
+				typeTab = 0;
+			});
+
+			$("#profile-tab").on("click", function(){
+				$("#budgetTable").css("display", "block")
+				$("#homeValueTable").css("display", "none");
+				typeTab = 1; 
+			});
+
+			$("#resetCalc").on("click", function(){
+				window.location.reload()
+			})
+
+		});
---
+++ js/util.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+
+	// $('#myTab li:first-child a').tab('show')
+
+	// $('#myTab a').on('click', function (e) {
+	// 	e.preventDefault()
+	// 	$(this).tab('show')
+	// })
+
+	// $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	// 	e.target // newly activated tab
+	// 	e.relatedTarget // previous active tab
+	// })
+
+})
